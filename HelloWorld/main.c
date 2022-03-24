5a749426428112a39ee241120e1f7dd5217a5f91
Waldi Febrianda <febrianda.waldi@gmail.com>
2022-03-24 08:32:38 +0700

    first commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+this is first program in c for beginners
+this program show hello world on the screen
+#include <stdio.h> : library stands for standard input and output
+
+main(){} file : its called method where we put our code
+printf : printf text into screen
+
+when running c program:
+1.we are building/ compiling : take all c code and translae it tinot language that comupter able to understand
+c program transle into comptuer code
+
+2. run the file get build
+
+
+
+
+
+*/
+int main()
+{
+    printf("Hello world!\n");
+    printf("             |\n");
+    printf("            /|\n");
+    printf("          /  |  \n");
+    printf("        /    |   \n");
+
+    return 0;
+}
