2808e6758291eb18327f4e8037fe09438a98b1a6
Waldi Febrianda <febrianda.waldi@gmail.com>
2022-03-24 10:00:19 +0700

    update on getting user input

--- Getting user input/main.c
+++ Getting user input/main.c
@@ -4,10 +4,28 @@
 int main()
 {
     int age;
+    double gpa;
+    char grade;
+    char name[20];
+
+    printf("what is complete name: ?");
+    fgets(name, 20, stdin);
+    //scanf("%s", name);
+    printf("your name is ");
+
     printf("Enter your age: ");
     scanf("%d", &age);
+    printf("you are %d years old, LOL\n", age);
 
-    printf("you are %d years old", age);
+    printf("Enter your gpa : ");
+    scanf("%lf", &gpa);
+    printf("your gpa is %f \n");
+
+    printf("what is your grade in c programming?: ");
+    scanf("%c", &grade);
+    printf("your grade is %c ", grade);
+
 
     return 0;
+
 }
